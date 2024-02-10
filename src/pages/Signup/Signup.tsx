e107940a537b3a09c95cff6955e4b97d4a6f0c67
Wesley McGhee <wesmcghee6372@gmail.com>
2024-02-09 19:55:54 -0500

    added login page

--- src/pages/Signup/Signup.tsx
+++ src/pages/Signup/Signup.tsx
@@ -10,6 +10,62 @@ interface ISignUpInputs {
   confirmPassword: string;
 }
 
+  const [inputs, setInputs] = useState<ISignUpInputs>({
+    username: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  function changeHandler(e: any) {
+    setInputs({ ...inputs, [e.target.name]: e.target.value });
+  }
+
+  function submitHandler(e: any) {
+    e.preventDefault();
+    if (inputs.password !== inputs.confirmPassword) {
+      console.log("Passwords don't match");
+    }
+    console.log(inputs);
+  }
+
+  return (
+    <div className="signup-page">
+      <Header />
+      <form className="form" onSubmit={(e) => submitHandler(e)}>
+        <h1>Sign Up</h1>
+        <input
+          className="form-el"
+          name="username"
+          type="text"
+          placeholder="Username"
+          onChange={(e) => changeHandler(e)}
+        />
+        <input
+          className="form-el"
+          name="email"
+          type="email"
+          placeholder="Email"
+          onChange={(e) => changeHandler(e)}
+        />
+        <input
+          className="form-el"
+          name="password"
+          type="password"
+          placeholder="Password"
+          onChange={(e) => changeHandler(e)}
+        />
+        <input
+          className="form-el"
+          name="confirmPassword"
+          type="password"
+          placeholder="Confirm Password"
+          onChange={(e) => changeHandler(e)}
+        />
+        <input className="form-btn" type="submit" />
+      </form>
+    </div>
+  );
 export default function Signup() {
   const [inputs, setInputs] = useState<ISignUpInputs>({
     username: "",
