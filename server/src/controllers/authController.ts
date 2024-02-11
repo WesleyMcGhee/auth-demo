5ee42745dbe0fe71a3a66a6ddf46bf5e8cf873d3
Wesley McGhee <wesmcghee6372@gmail.com>
2024-02-10 20:47:25 -0600

    added password hashing

--- server/src/controllers/authController.ts
+++ server/src/controllers/authController.ts
@@ -1,5 +1,9 @@
 import { Response, Request } from "express";
+import bcrypt from "bcrypt";
 import pool from "../database/db";
+
+// Probably not the actual one will use ENV var for this
+const userSaltRounds: number = 10;
 
 export async function postSignup(
   req: Request,
@@ -8,12 +12,22 @@ export async function postSignup(
   try {
     const { username, email, password } = req.params;
 
-    // Want to perform some validation at some point
-
-    // Ackshully make the database lol
-    await pool.query(
-      "INSERT INTO USERS (username, email, password) VALUES($1, $2, $3)"
+    const user = await pool.query(
+      "SELECT username, email FROM users WHERE username=$1 OR email=$2",
+      [username, email]
     );
+
+    if (user.rows.length > 0) {
+      // Do some more validiation for better error handling in the future
+      return res
+        .status(403)
+        .send("User with email, or username already exists");
+    }
+    bcrypt.hash(password, userSaltRounds, async () => {
+      await pool.query(
+        "INSERT INTO USERS (username, email, password) VALUES($1, $2, $3)"
+      );
+    });
 
     return res.status(202).send("Created");
   } catch (err) {
