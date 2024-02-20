7bba1b2c831898383030fba2dc58539a283aaa15
Wesley McGhee <wesmcghee6372@gmail.com>
2024-02-19 20:09:20 -0600

    added JWT to server

--- server/src/controllers/authController.ts
+++ server/src/controllers/authController.ts
@@ -1,4 +1,4 @@
-import { Response, Request } from "express";
+import { Response, Request, NextFunction } from "express";
 import bcrypt from "bcrypt";
 import pool from "../database/db";
 import * as jwt from "jsonwebtoken";
@@ -61,9 +61,32 @@ export async function postSignin(
       return res.status(400).send("Username or Password incorrect");
     }
 
-    return res.status(200).send("Authorized");
+    const userObj = { name: user.rows[0] };
+
+    if (!process.env.AUTH) {
+      console.error("AUTH SECRET NOT SET!!!");
+      return res.status(500).send("Internal Server Error");
+    }
+
+    const accessToken = jwt.sign(userObj, process.env.AUTH);
+
+    return res.status(200).json({ accessToken });
   } catch (err) {
     console.error(`Error in postSignin at ${new Date()}, ERROR: ${err}`);
     return res.status(500).send("Internal Server Error");
   }
+}
+
+function authenticateToken(req: any, res: Response, next: NextFunction) {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+  if (token == null) return res.sendStatus(401);
+  if (!process.env.AUTH_TOKEN_SECRET)
+    return res.status(500).send("Internal Server Error");
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET ?? "", (err, user) => {
+    if (err) return res.sendStatus(403);
+
+    req.user = user;
+    next();
+  });
 }
